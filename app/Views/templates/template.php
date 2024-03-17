dd625d6b9554731e07da6642030e58d7e88647f4
Matheus Henrique <matheuseevee@gmail.com>
2024-03-17 18:32:35 -0300

    Update template.php
    aplicado bootstrap, jquery e fontawesome

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -5,10 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Commerce</title>
+    <script src="<?= base_url('assets/bootstrap-5.3.3/js/bootstrap.js') ?>"></script>
+    <script src="<?= base_url('assets/fontawesome/js/all.js') ?>"></script>
+    <script src="<?= base_url('assets/jquery/jquery-3.7.1.js') ?>"></script>
 </head>
 
 <body>
     <?php $this->renderSection('conteudo'); ?>
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap-5.3.3/css/bootstrap.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/fontawesome/css/all.css') ?>">
 </body>
 
 </html>
